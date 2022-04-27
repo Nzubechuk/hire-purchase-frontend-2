49214878683478ddd041bf5f966363d474072b1e
mm3eoma <jchime20@gmail.com>
2022-04-27 12:40:39 -0700

    sign up

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'green-rgba': 'rgba(22,255,2,0.8)',
+        'red-rgba' : 'rgba(234,67,53,1)',
+        'blue-rgba': 'rgba(59,89,152,1)',
+      },
+    },
+  },
+  plugins: [],
+}
